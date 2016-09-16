2cadc9f4ace195d4be02639d00e9b44e177e6621
yiwenzhang92 <yiwenzhg@umich.edu>
2016-09-16 03:44:37 -0400

    first attempt on Scala

---
+++ examples/src/main/scala/org/apache/spark/examples/graphx/PageRankTwitter.scala
@@ -0,0 +1,28 @@
+//
+//  PageRankTwitter.scala
+
+import org.apache.spark._
+import org.apache.spark.graphx._
+import org.apache.spark.graphx.lib._
+
+
+object PageRankTwitter {
+    def main(args: Array[String]): Unit = {
+
+        val startTime = System.currentTimeMillis();
+
+        val graph = GraphLoader.edgeListFile(sc, "/home/ubuntu/edgein.txt");
+
+        val ranks = graph.staticPageRank(args(0)).vertices;
+
+        ranks.saveAsTextFile("/home/ubuntu/out_spark");
+
+        val elapsedTimeMillis = System.currentTimeMillis() - startTime;
+
+        println("Total latency: " + elapsedTimeMillis);
+
+        spark.stop();
+    }
+}
+
+
